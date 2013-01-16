3159946aca3f60d22f136f121029cfbf8e412a3a
Paul Eipper <paul@nkey.com.br>
2013-01-16 18:06:24 -0200

    Enable SSH agent forwarding for sudo

--- templates/ubuntu-12.04.1-server-i386-salt/postinstall.sh
+++ templates/ubuntu-12.04.1-server-i386-salt/postinstall.sh
@@ -31,7 +31,7 @@ rm VBoxGuestAdditions_$VBOX_VERSION.iso
 groupadd -r admin
 usermod -a -G admin vagrant
 cp /etc/sudoers /etc/sudoers.orig
-sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=admin' /etc/sudoers
+sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=admin\nDefaults\tenv_keep+="SSH_AUTH_SOCK"' /etc/sudoers
 sed -i -e 's/%admin ALL=(ALL) ALL/%admin ALL=NOPASSWD:ALL/g' /etc/sudoers
 
 # Install NFS client
